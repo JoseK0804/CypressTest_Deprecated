40a1d245186ff734faf755a78348a50ccd993f2b
JoseK0804 <josekarlosvz@gmail.com>
2025-09-08 20:39:39 -0300

    Reestructuré el proyecto

---
+++ Proyecto 1/cypress/integration/CP003.js
@@ -0,0 +1,41 @@
+// Importar las paginas
+import indexPage from "../support/Pages/index";
+import productPage from "../support/Pages/product";
+
+describe(`CP003`, ()=> {
+    // Se ejecuta antes de todas las pruebas
+    before(()=> {
+        cy.log(`Empiezan los test`);
+    })
+
+    // Se ejecuta despues de todas las pruebas
+    after(()=> {
+        cy.log(`Terminaron todos los test`);
+    })
+
+    // Se ejecuta antes de cada prueba
+    beforeEach(()=> {
+        cy.log(`Empezo el test`);
+        cy.visit(`/`);
+        cy.viewport(1000, 660);
+    })
+ 
+    // Se ejecuta despues de todas las pruebas
+    afterEach(()=> {
+        cy.log(`Se termino el test`);
+    })
+
+    // Primer test
+    it(`Validar cuotas en compra de equipo -Cuotas.60 -Equipo.Tercero de la lista -Banco.Credicoop -Tarjeta.Visa `, ()=> {
+        // Metodo para seleccionar el tercer producto de la lista
+        indexPage.selectThirdProduct();
+        // Metodo para entrar a las opciones de facturacion
+        productPage.billingOption();
+        // Metodo para seleccionar el banco y la tarjeta 
+        productPage.selectOption(`Credicoop`, `Visa`);
+        // Metodo para validar que no diga 60 en las cuotas
+        productPage.validarCuotas(`60`);
+        // Tomar captura de pantalla del resultado final
+        cy.screenshot('CP003')
+    });
+})
